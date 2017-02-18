59fecc0f0b2986420cdf328cafbb7e3bc40dc34e
leon <liangyaorong1995@outlook.com>
2017-02-18 20:59:00 +0800

    Update 1.1-user_pay_analysis.py

--- 1.1-user_pay_analysis.py
+++ 1.1-user_pay_analysis.py
@@ -34,7 +34,7 @@ def get_shop_pay_dict():
             shop_pay_dict[shop_id][date] = 0
     return shop_pay_dict
 
-####初始化{商家:{日期:支付数}} (只能一行一行地统计,因为内存不够)
+####统计{商家:{日期:支付数}} (只能一行一行地统计,因为内存不够)
 def get_shop_pay_count():
     days_list = get_date_list('2015-07-01','2016-10-31')
     shop_pay_count = get_shop_pay_dict()
